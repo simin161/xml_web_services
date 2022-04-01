8d9f032ca85057d940fdc2c1de0d487f8f7027fd
simin161 <shmepard@gmail.com>
2022-04-01 23:28:37 +0200

    user model

---
+++ SparkREST/src/beans/User.java
@@ -0,0 +1,13 @@
+package beans;
+
+import beans.enums.UserType;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String organizationName;
+    private String organizationUnit;
+    private String countryId;
+    private String email;
+    private UserType userType;
+}
