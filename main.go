d2ad98c2593b57d3c38abbab27552cdc760f9438
simin161 <shmepard@gmail.com>
2022-04-22 22:47:50 +0200

    successfully inserted user (registration "done")

--- main.go
+++ main.go
@@ -6,10 +6,20 @@ import (
 	"net/http"
 	"context"
 	"time"
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type user struct{
+	FirstName string
+	LastName string
+	Email string
+	Username string
+	Password string
+}
 
 func close(client *mongo.Client, ctx context.Context,
 	cancel context.CancelFunc){
@@ -65,6 +75,18 @@ func ping(client *mongo.Client, ctx context.Context) error{
 	return nil
 }
 
+func insertOne (client *mongo.Client, ctx context.Context, dataBase, col string, doc interface{}) (*mongo.InsertOneResult, error) {
+
+// select database and collection ith Client.Database method
+// and Database.Collection method
+collection := client.Database(dataBase).Collection(col)
+
+// InsertOne accept two argument of type Context
+// and of empty interface
+result, err := collection.InsertOne(ctx, doc)
+return result, err
+}
+
 func main() {
 
 	client, ctx, cancel, err := connect("mongodb://localhost:27017")
@@ -92,5 +114,38 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	//r.ParseForm()  baca error jer je sve ovo prazno
 	//x := r.Form.Get("firstName")
 	//fmt.Println(r.GetBody())
-	fmt.Println("AAAA")
+	client, ctx, cancel, err := connect("mongodb://localhost:27017")
+
+	decoder := json.NewDecoder(r.Body)
+	var userData user
+	err1 := decoder.Decode(&userData)
+	if err1 != nil {
+		panic(err1)
+	}
+
+	fmt.Println(userData)
+	var document interface{}
+
+	document = bson.D{
+		{"firstName", userData.FirstName},
+		{"lastName", userData.LastName},
+		{"username", userData.Username},
+		{"email", userData.Email},
+		{"password", userData.Password},
+	}
+
+	insertOneResult, err := insertOne(client, ctx, "gfg",
+		"marks", document)
+
+	// handle the error
+	if err != nil {
+		panic(err)
+	}
+
+	// print the insertion id of the document,
+	// if it is inserted.
+	fmt.Println("Result of InsertOne")
+	fmt.Println(insertOneResult.InsertedID)
+	defer close(client, ctx, cancel)
+
 }
